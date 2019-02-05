75bfefbc8e545a3fc67d3a638b0de7af88589880
LastEndConductor <42978769+LastEndConductor@users.noreply.github.com>
2019-02-04 21:33:14 -0500

    p1 installed the intended performance's prototype

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -9,9 +9,23 @@ This is a sisphean experience
 meant to emulate the sisphean struggle with a determined goal but no way to achieve said goal
 
 ******************/
+let x;
 
 $(document).ready(setup);
 
 function setup(){
-  
+setInterval(update,200);
+}
+
+function update(){
+  let x = $('input').val();
+  if(x==="sisyphus was here"){
+    $("#insult").text("cheater")
+  }
+  for(let i=0; i<x.length; i++){
+    if (x.substring(i,i+1)==="r"){
+      $('input').val("");
+      $("#insult").text("dumbass");
+    }
+  }
 }
