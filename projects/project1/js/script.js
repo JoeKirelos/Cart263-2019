5fe80b05412c0fb7499d564fa47ea06fb2addf2d
JOE KIRELOS <42978769+LastEndConductor@users.noreply.github.com>
2019-02-05 12:17:37 -0500

    p1 commented the code

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -7,53 +7,86 @@ Joe K
 
 This is a sisphean experience
 meant to emulate the sisphean struggle with a determined goal but no way to achieve said goal
+the player is supposed to type the words "sisyphus was here" in a text box in order to access a bigger game, which doesn't really exist as it is impossible to type those words in the box
+it shows the futility of trying by letting you type up but stopping you at the penultimate letter
 
 ******************/
-let x;
-let c;
-let b;
+
+// holding our variables here, for sound, arrays for insults and values for the arrays and the input
+//textInput is a variable to hold the input value of what the player types in the input box
+let textInput;
+//randomFailing and randomNotTrying are variables to be used to randomly generate an insult from an array based on the condition
+let randomNotTrying;
+let randomFailing;
+//count keeps track of how many times the player has messed up
 let count = 0;
-let insults1 = [];
-let insults2 = [];
+//an array to hold the insults if the player is not trying
+let notTrying = [];
+//an array to hold the insults if the player is trying and failing
+let failing = [];
+//an array to hold the sound for the ad popup
 let bloop;
+//when document is ready run setup
 $(document).ready(setup);
 
+//function setup()
+//
+//runs the things i need to have ran once
 function setup(){
+  //create a new audio to be placed inside variable bloop
   bloop = new Audio("assets/sounds/bloop.mp3");
-  insults1.push("Were you dropped on your head as a kid ? come on!");
-  insults1.push("How hard can it be it's only 3 words, a whole game awaits");
-  insults1.push("Oh my Zeus, do you even want to play this game ?");
-  insults2.push("the only 'r' you'll get is the one in 'restart' ;)");
-  insults2.push(":)");
-  insults2.push("can you RRepeat that?");
+  //pushing insults into both arrays
+  notTrying.push("Were you dropped on your head as a kid ? come on!");
+  notTrying.push("How hard can it be it's only 3 words, a whole game awaits");
+  notTrying.push("Oh my Zeus, do you even want to play this game ?");
+  failing.push("the only 'r' you'll get is the one in 'restart' ;)");
+  failing.push(":)");
+  failing.push("can you RRepeat that?");
+  //setting an interval for how often the update function runs (every 50 miliseconds)
   setInterval(update,50);
+  //tracks if the player hit enter
   $('input').on("keyup",function(event){
     let z = event.which;
     if(z===13){
-      if(x==="Sisyphus Was Here" || x==="sisyphus was here" || x==="Sisyphus was her"){
+      //if the player tried to copy paste the text they get a special insult
+      if(textInput==="Sisyphus Was Here" || textInput==="sisyphus was here" || textInput==="Sisyphus was her"){
         $('#insults').prepend(`<p>Foolish, you think Zeus would let Sisyphus get any assistance on his quest! Try Again!</p>`);
+        //after every attempt the player makes run resetNCount
         resetNCount();
     }else{
-      $('#insults').prepend(`<p>${insults1[b]}</p>`);
+      //if the player hit enter but hadn't copy pasted (i.e. they tried to write something else) give them an insult from the not trying array.
+      $('#insults').prepend(`<p>${notTrying[randomNotTrying]}</p>`);
+      //after every attempt the player makes run resetNCount
       resetNCount();
     }
   }
 });
 }
 
-
+//function update()
+//
+// runs every 50 miliseconds
 function update(){
-  c = Math.floor(Math.random()*insults2.length);
-  b = Math.floor(Math.random()*insults1.length);
-  x = $('input').val();
-  if(x==="Sisyphus Was Her" || x==="sisyphus was her" || x==="Sisyphus was her"){
-    $('#insults').prepend(`<p>${insults2[c]}</p>`);
+  //sets a random value for each of the arrays of insults
+  randomFailing = Math.floor(Math.random()*failing.length);
+  randomNotTrying = Math.floor(Math.random()*notTrying.length);
+  //holds the input value of what is typed in the input box
+  textInput = $('input').val();
+  //if the player is attempting to type sisyphus was here it will stop them before they make it to the last e
+  if(textInput==="Sisyphus Was Her" || textInput==="sisyphus was her" || textInput==="Sisyphus was her"){
+    //taunt the player for failing to type it
+    $('#insults').prepend(`<p>${failing[randomFailing]}</p>`);
+    //after every attempt the player makes run resetNCount
     resetNCount();
   }
 }
-
+//function resetNcount()
+//
+//function that runs every time the player makes an attempt
 function resetNCount(){
+  //resets the content of the input box
   $('input').val("");
+  //incrimentally increase the size of the image til it caps at 600px
   let padding = parseInt($('.banner').css('padding-top'));
   console.log(padding);
   padding+=50;
@@ -61,13 +94,18 @@ function resetNCount(){
     padding=600;
   }
   $('.banner').css('padding-top',padding+"px");
+  //keep count of how many tries the player has made up to 5
   count++;
+  //once the count hits 5, show the ad and play the bloop sound then reset count
   if (count===5){
     bloop.play();
+    //remove the hidden class off the div holding the ad and giving it the class of dialogue
     $('.hiddenAD').removeClass("hiddenAD").addClass("dialogue");
   }else if (count>5){
+    //once it gets more than 5 reset the count and give the ad back its hidden fucntion
     $('.dialogue').removeClass("dialogue").addClass("hiddenAD");
     count = 0;
   }
+  //make the div with class dialogue into a dialog box (which only happens once every 5-6 attempts)
   $('.dialogue').dialog();
 }
