38e322fd0ed937de42b86c8485304955d7348ca8
LastEndConductor <42978769+LastEndConductor@users.noreply.github.com>
2019-02-05 00:05:04 -0500

    p1 functionality  done, looks not even close

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -10,22 +10,35 @@ meant to emulate the sisphean struggle with a determined goal but no way to achi
 
 ******************/
 let x;
-
+let c;
+let insults = [];
 $(document).ready(setup);
 
 function setup(){
-setInterval(update,200);
+  insults.push("Were you dropped on your head as a kid ? come on!");
+  insults.push("How hard can it be it's only 3 words, a whole game awaits");
+  insults.push("Oh my Zeus, do you even want to play this game ?");
+  insults.push("the only 'r' you'll get is the one in 'restart' ;)");
+  insults.push(":)");
+  insults.push("can you repeat that?");
+  setInterval(update,200);
+
 }
 
 function update(){
+  c = Math.floor(Math.random()*Math.floor(7));
   let x = $('input').val();
-  if(x==="sisyphus was here"){
-    $("#insult").text("cheater")
-  }
+  if(x==="Sisyphus Was Here"){
+    $("#insult").text("Foolish, you think Zeus would let Sisyphus get any assistance on his quest! Try Again!")
+    $('input').val("");
+  }else{
   for(let i=0; i<x.length; i++){
     if (x.substring(i,i+1)==="r"){
+      $("div").after(function(){
+        return "<p>" + insults[c] + "</p>"
+      });
       $('input').val("");
-      $("#insult").text("dumbass");
     }
   }
+}
 }
