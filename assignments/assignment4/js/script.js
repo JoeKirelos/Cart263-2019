45671c69e28924c3b62762e0077a9d65532c964e
LastEndConductor <42978769+LastEndConductor@users.noreply.github.com>
2019-02-12 18:09:06 -0500

    assignment 4 got the droppable to reject but not reversable yet

--- assignments/assignment4/js/script.js
+++ assignments/assignment4/js/script.js
@@ -21,50 +21,34 @@ let crunchSFX = new Audio("assets/sounds/crunch.wav");
 // Variable to hold our two key elements
 let $mouth;
 let $fly;
+let $cookie;
 
 $(document).ready(setup);
 
 function setup() {
   // Get the mouth element from the page
   $mouth = $('#mouth');
-  // Make it droppable
-  $mouth.droppable({
-    // The drop option specifies a function to call when a drop is completed
-    drop: flyDropped
-  });
-
-  // Get the fly element from the page
   $fly = $('#fly');
-  // Make it draggable
-  $fly.draggable();
-
-  // Start up the buzzing of the fly
+  $cookie = $('#cookie');
   buzzSFX.loop = true;
   buzzSFX.play();
+
+  $cookie.draggable({ revert: "valid" });
+  $fly.draggable({ revert: "invalid" });
+
+  $mouth.droppable({
+    accept: $fly,
+    drop: function(event,ui) {
+        ui.draggable.remove();
+        $(this).attr('src','assets/images/mouth-closed.png');
+        buzzSFX.pause();
+        crunchSFX.play();
+        setInterval(chew,250);
+      }
+  });
 }
 
-// flyDropped(event,ui)
-//
-// Called when a draggable element is dragged over the droppable element (the mouth)
-// In this instance it can only be the fly (it's the only draggable element).
-// The arguments 'event' and 'ui' are automatically passed by jQuery UI and contain
-// helpful information about the event.
-function flyDropped (event,ui) {
-  // When we drop the fly into the mouth we should remove the fly from the page
-  // ui contains a reference to the draggable element that was just dropped in ui.draggable
-  // .remove() removes the select element from the page
-  ui.draggable.remove(); // $fly.remove() would work here too
-  // We should "close the mouth" by changing its image
-  // .attr() lets us change specific attributes on HTML element by specifying the attribute
-  // and then what we want to set it to - in this case the 'src' attribute to the closed image
-  $(this).attr('src','assets/images/mouth-closed.png');
-  // Now the fly is gone we should stop its buzzing
-  buzzSFX.pause();
-  // And start the crunching sound effect of chewing
-  crunchSFX.play();
-  // Use a setInterval to call the chew() function over and over
-  setInterval(chew,250);
-}
+
 
 // chew()
 //
@@ -83,4 +67,8 @@ function chew () {
     // so we swap it for the open mouth
     $mouth.attr('src','assets/images/mouth-open.png');
   }
+}
+
+function eww () {
+
 }
