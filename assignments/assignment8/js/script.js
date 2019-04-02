36a914c1420163c7dcc602be79085ab64ecc6ed8
LastEndConductor <42978769+LastEndConductor@users.noreply.github.com>
2019-04-01 22:30:24 -0400

    a8 experimenting a bit with phaser, got a couple of animations working with a free use sprite

--- assignments/assignment8/js/script.js
+++ assignments/assignment8/js/script.js
@@ -1,9 +1,54 @@
 /*****************
 
-Title of Project
-Author Name
+prototype for project 3
+Joe
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+prototype for a side scrolling endless runner game
 
+assets used sorlo by doudoulolita
+https://opengameart.org/content/sorlo-ultimate-smash-friends
 ******************/
+let config = {
+  type: Phaser.AUTO,
+  width: 400,
+  height: 300,
+  scene: {
+    preload: preload,
+    create: create
+  }
+};
+
+let game = new Phaser.Game(config);
+
+function preload() {
+  this.load.spritesheet('sorloWalk', 'assets/images/sorloWalk.png', { frameWidth: 70, frameHeight: 70 });
+  this.load.spritesheet('sorloCast','assets/images/sorloCast.png', { frameWidth: 70, frameHeight: 70 });
+  // this.load.image('bg','assets/images/bg.jpg');
+  // this.load.image('circle','assets/images/circle.png');
+}
+
+function create () {
+  // this.image = this.add.image(400,300,'bg');
+  let playerCast = this.anims.create({
+    key: 'cast',
+    frames: this.anims.generateFrameNumbers('sorloCast'),
+    frameRate: 6,
+    repeat:0
+  });
+  let playerWalk = this.anims.create({
+    key: 'walk',
+    frames: this.anims.generateFrameNumbers('sorloWalk'),
+    frameRate: 4,
+    repeat: 10000000000000
+  });
+  let sprite = this.add.sprite(125,225,'sorloWalk');
+
+    this.input.on('pointerdown', function(event){
+      sprite.play('cast');
+      sprite.anims.chain('walk')
+    });
+    // this.input.on('pointerup', function(event){
+    //   sprite.play('walk');
+    // });
+    sprite.play('walk');
+}
