01eecd35cac914cb7bda9e4a550c5c7fd16be0df
LastEndConductor <42978769+LastEndConductor@users.noreply.github.com>
2019-04-02 17:18:21 -0400

    a8 some of the functionality is still missing, (player cant burn the zombie and zombie cant harm the player yet)

--- assignments/assignment8/js/script.js
+++ assignments/assignment8/js/script.js
@@ -19,18 +19,25 @@ let config = {
     default: 'arcade',
     arcade : {
       gravity : { y : 300},
-      debug: true
+      debug: false
     }
   },
   scene: {
     preload: preload,
-    create: create
+    create: create,
+    update: update
   }
 };
 
 let game = new Phaser.Game(config);
-
+let zSpeed = 20;
+let zombie;
+let player;
+let interaction = false;
+let counter = 0;
 function preload() {
+  this.load.spritesheet('zombieAttack','assets/images/zombieAttack1.png',{frameWidth: 70, frameHeight: 70});
+  this.load.spritesheet('zombieWalk','assets/images/zombieWalk.png',{frameWidth:70, frameHeight:70});
   this.load.spritesheet('sorloJump','assets/images/sorloJump.png',{frameWidth: 70, frameHeight: 70});
   this.load.spritesheet('tiles','assets/images/tiles.png', {frameWidth : 400, frameHeight: 34});
   this.load.spritesheet('sorloBackCast','assets/images/sorloBackCast.png', {frameWidth: 70, frameHeight: 70 });
@@ -46,12 +53,26 @@ function create () {
     frameRate: 3,
     repeat: -1
   });
+
+  let zombieAttacking = this.anims.create({
+    key: 'zombAttack',
+    frames: this.anims.generateFrameNumbers('zombieAttack'),
+    frameRate: 8,
+    repeat: 1
+  })
   let playerJump = this.anims.create({
     key: 'jump',
     frames: this.anims.generateFrameNumbers('sorloJump'),
     frameRate:3,
     repeat: 0
   });
+
+  let zombieWalking = this.anims.create({
+    key: 'zombWalk',
+    frames: this.anims.generateFrameNumbers('zombieWalk'),
+    frameRate:8,
+    repeat: -1
+  })
 
   let playerCast = this.anims.create({
     key: 'cast',
@@ -79,7 +100,9 @@ function create () {
     frames: this.anims.generateFrameNumbers('fire'),
     frameRate: 4,
     repeat: 0
+
   });
+
   let platform = this.physics.add.staticGroup();
 
     platform.create(200,286,'tiles').setScale(1).refreshBody();
@@ -88,15 +111,28 @@ function create () {
 
   road.play('ground');
 
-  let player = this.physics.add.sprite(125,225,'sorloWalk');
+  player = this.physics.add.sprite(125,225,'sorloWalk');
+  zombie = this.physics.add.sprite(380,225,'zombieWalk');
 
+if(interaction === true){
+  console.log('int')
+  zombie.play('zombAttack');
+}else{
+  zombie.play('zombWalk');
+}
+  zombie.setBounce(0.2);
   player.setBounce(0.2);
   player.setCollideWorldBounds(true);
 
   this.physics.add.collider(player, platform);
+  this.physics.add.collider(zombie, platform);
+  this.physics.add.collider(zombie,fire);
+
+
 
   this.input.on('pointerdown', (event) => {
-    let fire = this.add.sprite(event.x,event.y,'blaze');
+    let fire = this.physics.add.sprite(event.x,event.y,'blaze');
+    this.physics.add.collider(fire,platform);
     fire.on('animationcomplete', function(animation,frame){
       fire.destroy();
     });
@@ -119,4 +155,13 @@ function create () {
     })
   })
   player.play('walk');
+}
+
+ function update () {
+   zombie.setVelocityX(-20);
+   if (zombie.x-35<player.x && counter === 0){
+     zombie.play('zombAttack');
+     counter++;
+   }
+   zombie.anims.chain('zombWalk');
 }
